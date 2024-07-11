c6f14c64ddaa41dd7503feccf33e0be0d7167144
P442-byte <139857861+P442-byte@users.noreply.github.com>
2024-07-11 07:47:02 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <iframe className="ml-10 border-0" title="Mishi - The Mushroom Man" frameborder="0" src="https://itch.io/embed-upload/10884946?color=333333" allowfullscreen="" width="860" height="570"><a href="https://pf446.itch.io/mishi-the-mushroom-man">Play Mishi - The Mushroom Man on itch.io</a></iframe>
+      <iframe height="167" frameborder="0" src="https://itch.io/embed/2817988" width="552"><a href="https://pf446.itch.io/mishi-the-mushroom-man">Mishi - The Mushroom Man by pf446</a></iframe>
     </main>
   );
 }
